05b17c8a2dc3a40017955ca96b5d6056197308fd
Alexander Justine Santiago <Alexcode0702@gmail.com>
2026-06-02 22:59:56 +0700

    Update v0.1.0

--- app/api/combobox/schedule.ts
+++ app/api/combobox/schedule.ts
@@ -4,11 +4,11 @@ import { getClassScheduleList, getTrainerScheduleList } from "../schedule";
 export enum DayOfWeek {
   Sunday = 0,
   Monday = 1,
-  Tuesday = 2,
-  Wednesday = 3,
-  Thursday = 4,
-  Friday = 5,
-  Saturday = 6,
+  Tuesday = 3,
+  Wednesday = 4,
+  Thursday = 5,
+  Friday = 6,
+  Saturday = 7,
 }
 
 export async function getClassScheduleCombobox(payload?: {
