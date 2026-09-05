d1a60c890d1384907733a370dc6d1512225a99e1
Vovella <vovella@rsmurniteguh.com>
2026-09-05 09:14:25 +0700

    fix: payload birth-date (-tz)

--- utils/datetimeFormat.ts
+++ utils/datetimeFormat.ts
@@ -19,3 +19,9 @@ export function formatDate(dateString?: string): string {
 
   return `${day}/${month}/${year}`;
 }
+
+export function toYmd(date: Date): string {
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${date.getFullYear()}-${month}-${day}`;
+}
