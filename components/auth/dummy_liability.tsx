895cacb341dd35ce3a47196dda5e11732a12d4e6
Alexander Justine Santiago <Alexcode0702@gmail.com>
2026-09-24 23:09:12 +0700

    feat: TnC update

--- components/auth/dummy_liability.tsx
+++ components/auth/dummy_liability.tsx
@@ -1,66 +1,39 @@
 export const DUMMY_LIABILITY = {
-  title: "Mega Fitness Center Liability Form",
+  title: "PHYSICAL ACTIVITY READINESS LIABILITY FORM",
   sections: [
     {
-      title:
-        "1. PENGABAIAN KEWAJIBAN DAN PELEPASAN KLAIM (LIABILTY WAIVER  AND RELEASE OF CLAIMS)\n\n1A. PENGAKUAN RESIKO KOVID-19 ",
-      body: `Saya menyadari bahwa potensi resiko paparan virus Corona/COVID-19 masih ada dan oleh karena itu saya secara pribadi akan mematuhi semua peraturan yang ditetapkan oleh MEGA FITNESS CENTER demi mengurangi penyebaran virus Corona/COVID-19.`,
+      title: "Pengakuan Resiko (Acknowledgement of Risks)",
+      body: `Saya memahami dan menyadari bahwa aktivitas di MEGA FITNESS CENTER, termasuk penggunaan alat angkat beban, peralatan olahraga apa pun (mekanik atau lainnya), mesin kardiovaskular, atau area lain, ruang ganti, tempat parkir, tangga, kolam renang, kolam pusaran air hangat, sauna, cold plunge, lobi, lounge atau area umum lainnya dari fasilitas apa pun, atau peralatan apa pun. Termasuk juga mengikuti kelas, acara dan sesi pelatihan pribadi, dapat mengandung risiko bawaan, baik saat melakukan latihan fisik atau tidak. Risiko ini meliputi namun tidak terbatas pada: cedera otot, keseleo, serangan jantung, kerusakan alat, hingga cedera serius atau kematian.
+
+Sehubungan dengan penggunaan peralatan dan fasilitas serta mengikuti sesi kelas, pribadi dan acara yang disediakan oleh MEGA FITNESS CENTER, saya menyetujui bahwa MEGA FITNESS CENTER tidak bertanggung jawab atas segala kerugian yang timbul dari cedera pribadi (termasuk kematian) yang diderita oleh saya, atau tamu saya di sekitar lokasi latihan, atau sebagai akibat dari penggunaan peralatan atau fasilitas, terlepas dari apakah kerugian tersebut seluruhnya atau sebagian disebabkan oleh kelalaian MEGA FITNESS CENTER.`,
     },
     {
-      title: "INFORMASI KESELAMATAN COVID-19:",
-      body: `Saat berlatih/activitas di fasilitas atau berpartisipasi dalam acara yang diadakan atau disponsori oleh MEGA FITNESS CENTER ("MFC"). "Jaga jarak / social distancing" harus dipraktekkan dan penutup wajah yang dikenakan setiap saat (kecuali selama sesi kelas / pelatihan) untuk mengurangi risiko paparan COVID- 19 di karenakan COVID-19 sangat menular dan menyebar terutama dari kontak orang ke orang, MFC telah menerapkan langkah-langkah pencegahan untuk mengurangi penyebaran COVID-19. Namun, MFC tidak dapat menjamin bahwa para peserta, sukarelawan, mitra, atau orang lain tidak akan terinfeksi COVID-19. 
-      
-Mengingat penyebaran COVID-19 yang sedang berlangsung, individu yang termasuk dalam salah satu kategori di bawah ini tidak boleh terlibat dalam acara MFC dan / atau kegiatan pelatihan tatap muka lainnya. Dengan menghadiri acara MFC, Saya menyatakan bahwa Saya tidak termasuk dalam salah satu kategori berikut:
-
-1. Individu yang saat ini atau dalam empat belas (14) hari terakhir telah mengalami gejala yang terkait dengan COVID-19, yang meliputi demam, batuk, dan sesak napas antara lain;
-
-2. Individu yang telah melakukan perjalanan kapan saja dalam empat belas (14) hari terakhir baik secara internasional maupun ke masyarakat di Indonesia. yang telah mengalami atau sedang mengalami penyebaran COVID-19 yang berkelanjutan;`,
+      title: "Konfirmasi Kesehatan dan Kondisi Fisik (Health Confirmation)",
+      body: `Saya menyatakan bahwa saya dalam kondisi kesehatan yang baik dan mampu secara fisik untuk melakukan aktivitas olahraga. Saya setuju untuk berkonsultasi dengan dokter sebelum memulai program latihan apa pun jika saya memiliki riwayat penyakit jantung, masalah persendian, atau kondisi medis lainnya. Saya setuju untuk memberi tahu staf MEGA FITNESS CENTER jika ada keluhan kesehatan atau perubahan kondisi fisik selama berada di area MEGA FITNESS CENTER.`,
     },
     {
-      title: "TUGAS UNTUK SELF-MONITOR",
-      body: `Peserta setuju untuk memantau diri sendiri dan gejala COVID-19 (gejala biasanya termasuk demam, batuk, dan sesak napas) dan, hubungi MFC di Customer Relationship (0811-658-1238), IG: @mfc.fitness dan tik tok mfc.fitness7, jika ia mengalami gejala COVID-19 dalam waktu 14 hari setelah berpartisipasi atau menjadi sukarelawan dengan ACS.`,
+      title: "Pelepasan Tanggung Jawab (Release of Liability)",
+      body: `Saya mengakui bahwa saya memperoleh kepuasan pribadi dan manfaat berdasarkan partisipasi saya dan / atau sukarela dengan MEGA FITNESS CENTER, dan saya dengan sukarela terlibat dalam acara MEGA FITNESS CENTER dan / atau kegiatan pelatihan lainnya ("Aktivitas") di fasilitas MEGA FITNESS CENTER.
+
+Saya setuju atas nama diri saya sendiri (dan seluruh perwakilan pribadi Anda, ahli waris, pelaksana, administrator, agen, dan penugasan) untuk melepaskan dan memberhentikan Pelatih (dan afiliasi Pelatih, entitas terkait, karyawan, agen, perwakilan, penerus, dan penugasan) dari semua klaim atau penyebab tindakan (yang diketahui atau tidak diketahui) yang timbul karena kelalaian, baik aktif atau pasif, atau afiliasi Pelatih karyawan, agen, perwakilan, penerus, dan penugasan.
+
+Pengabaian dan pelepasan tanggung jawab ini mencakup, tanpa batasan, cedera yang mungkin terjadi sebagai akibat dari (a) penggunaan peralatan olahraga atau fasilitas yang mungkin tidak berfungsi atau rusak oleh Anda, (b) pemeliharaan peralatan olahraga yang tidak tepat, tempat atau fasilitas, (c) instruksi atau pengawasan yang lalai, termasuk pelatihan pribadi, (d) kelalaian dalam mempekerjakan atau menahan karyawan, dan/atau (e) terpeleset atau tersandung dan jatuh saat berada di bagian mana pun dari tempat atau saat bepergian ke atau dari pelatihan pribadi, termasuk cedera akibat kelalaian Inspeksi atau pemeliharaan fasilitas atau tempat oleh Pelatih atau orang lain.`,
     },
     {
-      title:
-        "PENGABAIAN KEWAJIBAN DAN PELEPASAN KLAIM (LIABILITY WAIVER AND RELEASE OF CLAIMS):",
-      body: `Saya mengakui bahwa saya memperoleh kepuasan pribadi dan manfaat berdasarkan partisipasi saya dan / atau sukarela dengan MFC, dan saya dengan sukarela terlibat dalam acara MFC dan / atau kegiatan pelatihan lainnya ("Aktivitas") di fasilitas MFC.`,
-    },
-
-    {
-      title: "PEMBEBASAN DAN PENGABAIAN (RELEASE AND WAIVER):",
-      body: `DENGAN INI SAYA MELEPASKAN, MELEPASKAN DAN SELAMANYA MELEPASKAN SETIAP DAN SEMUA KEWAJIBAN, KLAIM, DAN TUNTUTAN APA PUN JENIS ATAU SIFAT TERHADAP MEGA FITNESS CENTER, MITRA AFILIASINYA DAN SPONSOR, TERMASUK DALAM SETIAP KASUS, TANPA BATASAN, DIREKTUR MEREKA, MANAGER, COACH, INSTRUCTOR, PETUGAS, KARYAWAN, RELAWAN, DAN AGEN ("PIHAK YANG DIBEBASKAN"), BAIK DALAM HUKUM ATAU DALAM EKUITAS, SEJAUH DIIZINKAN OLEH HUKUM, TERMASUK NAMUN TIDAK TERBATAS PADA KERUSAKAN ATAU KERUGIAN YANG DISEBABKAN OLEH KELALAIAN, KESALAHAN ATAU PERILAKU APA PUN PADA BAGIAN DARI PIHAK YANG DI BEBASKAN (MEGA FITNESS CENTER) TERMASUK NAMUN TIDAK TERBATAS PADA KEMATIAN, CEDERA TUBUH, PENYAKIT, KERUGIAN  EKONOMI  ATAU KEHILANGAN  ATAU  KEHILANGAN DARI BIAYA SAKU SENDIRI, ATAU KEHILANGAN ATAU KERUSAKAN PROPERTI, YANG SAYA SEBAGAI , AHLI WARIS, PENERIMA TUGAS, KERABAT TERDEKAT DAN / ATAU PERWAKILAN YANG DITUNJUK SECARA HUKUM ATAU MENUNJUKAN, MUNGKIN MEMILIKI ATAU YANG SELANJUTNYA MUNGKIN TIMBUL  ATAS NAMA SAYA, ATAU MUNGKIN SELANJUTNYA TIMBUL DARI PARTISIPASI SAYA DENGAN KEGIATAN TERSEBUT.`,
+      title: "DENGAN INI SAYA MELEPASKAN SEMUA KEWAJIBAN, KLAIM, DAN TUNTUTAN",
+      body: `SAYA MELEPASKAN SEMUA KEWAJIBAN, KLAIM, DAN TUNTUTAN APA PUN JENIS ATAU SIFAT TERHADAP MEGA FITNESS CENTER, MITRA AFILIASI MEREKA DAN SPONSOR, TERMASUK DALAM SETIAP KASUS, TANPA BATASAN, DIREKTUR, MANAGER, COACH, INSTRUCTOR, PETUGAS, KARYAWAN, RELAWAN, DAN AGEN ("PIHAK YANG DIBEBASKAN"), BAIK DALAM HUKUM ATAU DALAM EKUITAS, SEJAUH DIIZINKAN OLEH HUKUM, TERMASUK NAMUN TIDAK TERBATAS PADA KERUSAKAN ATAU KERUGIAN YANG DISEBABKAN OLEH KELALAIAN, KESALAHAN ATAU PERILAKU APA PUN PADA BAGIAN DARI PIHAK YANG DIBEBASKAN TERMASUK NAMUN TIDAK TERBATAS PADA KEMATIAN, CEDERA TUBUH, PENYAKIT, KERUGIAN EKONOMI ATAU KEHILANGAN ATAU DARI BIAYA SAKU SENDIRI, ATAU KEHILANGAN ATAU KERUSAKAN PROPERTI.`,
     },
     {
-      title: "ASUMPSI RISIKO",
-      body: `Saya mengakui dan memahami hal-hal berikut:
+      title: "Persetujuan Medis Darurat (Medical Treatment)",
+      body: `Jika terjadi cedera, kecelakaan, atau kondisi darurat medis saat saya berada di area MEGA FITNESS CENTER, saya memberikan izin kepada staf MEGA FITNESS CENTER untuk memberikan pertolongan pertama atau memanggil bantuan medis yang dianggap perlu.
 
-1. Partisipasi mencakup kemungkinan paparan dan penyakit dari penyakit menular termasuk namun tidak terbatas pada COVID-19. Sementara aturan tertentu dan disiplin pribadi dapat mengurangi risiko ini, risiko penyakit serius dan kematian memang ada;
-
-2. Saya dengan sadar dan bebas menanggung semua risiko yang terkait dengan penyakit dan penyakit menular, seperti COVID- 19, bahkan jika timbul dari kelalaian atau kesalahan Pihak yang Dirilis; dan 
-
-3. Saya dengan ini dengan sadar mengasumsikan risiko cedera, kerusakan dan kerugian yang terkait dengan Aktivitas, termasuk cedera, kerusakan dan kerugian yang disebabkan oleh kelalaian, kesalahan atau perilaku apa pun dari pihak Pihak yang dirilis.
-`,
+Saya mengakui risiko kesehatan yang terkait dengan Aktivitas, termasuk namun tidak terbatas pada pusing sementara, pusing, pingsan, mual, kram otot, cedera muskuloskeletal, nyeri sendi, keseleo dan ketegangan, serangan jantung, stroke, atau kematian mendadak. Saya setuju bahwa jika saya mengalami salah satu dari ini atau gejala lainnya selama kegiatan, saya akan segera menghentikan latihan/partisipasi saya dan mencari perhatian medis yang tepat. SAYA DENGAN INI MELEPASKAN DAN SELAMANYA MEMBEBASKAN PIHAK YANG DIBEBASKAN DARI KLAIM APA PUN YANG MUNCUL ATAU MUNGKIN SELANJUTNYA TIMBUL KARENA PERTOLONGAN PERTAMA, PERAWATAN, ATAU LAYANAN YANG DIBERIKAN SEHUBUNGAN DENGAN PARTISIPASI SAYA DALAM KEGIATAN TERSEBUT.`,
     },
     {
-      title:
-        "2. PENGAKUAN MEDIS DAN PELEPASAN (MEDICAL ACKNOWLEDGMENT AND RELEASE)",
-      body: `Saya mengakui risiko kesehatan yang terkait dengan Aktivitas, termasuk namun tidak terbatas pada pusing sementara, pusing, pingsan, mual, kram otot, cedera muskuloskeletal, nyeri sendi, keseleo dan ketegangan, serangan jantung, stroke, atau kematian mendadak. Saya  setuju bahwa jika saya mengalami salah satu dari ini atau gejala lainnya selama kegiatan, saya akan segera menghentikan latihan/partisipasi saya dan mencari perhatian medis yang tepat. SAYA DENGAN INI MELEPASKAN DAN SELAMANYA MEMBEBASKAN PIHAK YANG DIBEBASKAN DARI KLAIM APA PUN YANG MUNCUL ATAU MUNGKIN SELANJUTNYA TIMBUL KARENA PERTOLONGAN PERTAMA, PERAWATAN, ATAU LAYANAN YANG DIBERIKAN SEHUBUNGAN DENGAN PARTISIPASI SAYA DALAM KEGIATAN TERSEBUT.
+      title: "Persetujuan dan Kesepakatan",
+      body: `Saya setuju untuk mematuhi semua peraturan yang diberlakukan oleh MEGA FITNESS CENTER mengenai penggunaan fasilitas dan peralatan. Saya setuju untuk selalu berperilaku terkendali dan wajar, dan tidak menggunakan peralatan apa pun dengan cara yang tidak sesuai dengan desain dan tujuannya.
 
-Sebagai peserta, sukarelawan, atau peserta, Saya menyadari bahwa partisipasi, keterlibatan, dan  atau kehadiran Saya di acara atau aktivitas MEGA FITNESS CENTER ("Aktivitas") bersifat sukarela dan dapat mengakibatkan cedera pribadi (termasuk kematian) dan / atau kerusakan properti. Dengan menghadiri, mengamati atau berpartisipasi dalam Kegiatan, Saya mengakui dan menanggung semua risiko dan bahaya yang terkait dengan partisipasi dan / atau kehadiran Saya di Kegiatan, dan Saya setuju bahwa: (a) MEGA FITNESS CENTER (b) pemilik properti atau situs dari Kegiatan, dan (c) semua afiliasi masa lalu, sekarang dan masa depan, penerus, menugaskan, karyawan, relawan, vendor, mitra, direktur, dan petugas, dari Intansi tersebut (sub bagian (a) melalui (c), secara kolektif, "Pihak yang Dirilis"), akan tidak bertanggung jawab atas cedera pribadi (termasuk kematian), kerusakan properti, atau kerugian lainnya yang diderita sebagai akibat dari partisipasi Saya dalam, kehadiran di, dan / atau pengamatan Kegiatan, terlepas dari apakah cedera atau kerugian tersebut disebabkan oleh kelalaian salah satu Pihak yang Dirilis (secara kolektif, "Klaim yang Dirilis"). DENGAN MENGHADIRI DAN / ATAU BERPARTISIPASI DALAM KEGIATAN TERSEBUT, SAYA DIANGGAP TELAH MEMBERIKAN PELEPASAN PENUH TANGGUNG JAWAB KEPADA PIHAK YANG DIBEBASKAN SEJAUH DIIZINKAN OLEH HUKUM.
-
-PARA PIHAK setuju untuk mencari resolusi informal untuk setiap kemungkinan perselisihan sebelum mengejar solusi hukum. Dalam hal kesepakatan bersama tidak dapat dicapai, akhirnya akan diselesaikan oleh BANI (Badan Arbitrase Nasional Indonesia) dan di bawah hukum Indonesia. Keputusan oleh arbiter harus bersifat final secara hukum dan mengikat secara hukum bagi PARA PIHAK,
-`,
-    },
-    {
-      title: "3. RESIKO PERSONAL DAN PENGGUNAAN ALAT-ALAT GYM",
-      body: `Sehubungan dengan penggunaan peralatan dan fasilitas yang disediakan oleh MEGA FITNESS CENTER, saya menyetujui bahwa MEGA FITNESS CENTER tidak bertanggung jawab atas segala kerugian yang timbul dari cedera pribadi (termasuk kematian) yang diderita oleh saya, atau tamu saya di sekitar lokasi latihan, atau sebagai akibat dari penggunaan peralatan atau fasilitas, terlepas dari apakah kerugian tersebut seluruhnya atau sebagian disebabkan oleh kelalaian MEGA FITNESS CENTER.
-
-Dengan menyepakati perjanjian ini, saya menerima dan bertanggung jawab penuh atas setiap dan seluruh cedera, kerusakan (baik ekonomi maupun non-ekonomi), dan kerugian dalam bentuk apa pun, yang mungkin terjadi pada saya atau tamu saya, dan saya dengan ini sepenuhnya dan selamanya melepaskan dan membebaskan perusahaan, perusahaan asuransi, karyawan, pejabat, direktur, dan asosiasinya, dari setiap dan seluruh tuntutan, tuntutan, kerusakan, hak untuk melakukan tindakan, atau penyebab tindakan, saat ini atau di masa depan, baik yang diketahui atau tidak diketahui, atau tidak diantisipasi, atau tidak terduga, yang diakibatkan atau ditimbulkan dari penggunaan peralatan dan fasilitas tersebut.
-
-Saya setuju untuk mematuhi semua peraturan yang diberlakukan oleh MEGA FITNESS CENTER mengenai penggunaan fasilitas dan peralatan. Saya setuju untuk selalu berperilaku terkendali dan wajar, dan tidak menggunakan peralatan apa pun dengan cara yang tidak sesuai dengan desain dan tujuannya.
-
-SAYA TELAH MEMBACA PENGECUALIAN DAN PEMBEBASAN TANGGUNG JAWAB DI ATAS DAN SECARA SUKARELA MENYEPAKATI DAN MENJALANKAN PERJANJIAN INI DENGAN PENGETAHUAN DAN KESADARAN PENUH TENTANG ISINYA.
-`,
+Dengan menandatangani formulir ini, saya menyatakan bahwa saya telah membaca, memahami, dan menyetujui seluruh syarat dan ketentuan di dalam dokumen ini secara sukarela, serta menyadari bahwa saya telah melepaskan hak hukum tertentu.`,
     },
   ],
 };
